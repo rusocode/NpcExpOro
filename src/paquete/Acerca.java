a6fdf0e425c2ce898d218a811fc9cc94e57e3417
Juan Debenedetti <juandebenedetti94@gmail.com>
2020-08-03 11:01:46 -0300

    Add files via upload

---
+++ src/paquete/Acerca.java
@@ -0,0 +1,79 @@
+package paquete;
+
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import net.miginfocom.swing.MigLayout;
+
+public class Acerca extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	private JPanel panel;
+	private JTextArea txtArea;
+	private JButton btnOk;
+	private JHyperlink link;
+
+	private static Acerca instance;
+
+	public static Acerca getInstance() {
+		if (instance == null) instance = new Acerca();
+		return instance;
+	}
+
+	private Acerca() {
+
+		setTitle("Acerca de CalculadoraAO");
+
+		setResizable(false);
+
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+		initialize();
+	}
+
+	private void initialize() {
+		setLayout(new MigLayout());
+
+		panel = new JPanel();
+		panel.setLayout(new MigLayout("insets 0", "[][grow, right]"));
+
+		txtArea = new JTextArea();
+		txtArea.setEditable(false);
+		txtArea.setBackground(SystemColor.menu);
+		txtArea.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		String str = "Calculadora multiplataforma basada en ImperiumAO que calcula el porcentaje"
+				+ "\nde experiencia que otorga el NPC, la cantidad de NPCs a matar para pasar de" + "\nnivel y el oro total. " + "\n\nVersión: 1.0"
+				+ "\nCopyright © 2020 Juan Debenedetti" + "\nAll rights reserved.";
+		txtArea.append(str);
+		add(txtArea, "wrap");
+
+		btnOk = new JButton("OK");
+		btnOk.setFocusable(false);
+		btnOk.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				if (evt.getSource() == btnOk) dispose(); // Cierra la ventana actual y libera los recursos que esa ventana haya estado ocupando
+			}
+		});
+		panel.add(btnOk);
+
+		link = new JHyperlink("github.com/CalculadoraAO");
+		link.setURL("https://github.com/rusocode/CalculadoraAO");
+		panel.add(link);
+
+		add(panel, "spanx, growx");
+
+		pack();
+		setLocationRelativeTo(null);
+
+	}
+
+}
---
+++ src/paquete/JHyperlink.java
@@ -0,0 +1,67 @@
+package paquete;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class JHyperlink extends JLabel {
+
+	private static final long serialVersionUID = 1L;
+
+	private String url;
+
+	public JHyperlink(String txt) {
+		// Le pasa el texto (nombre) al constructor de la clase JLabel
+		super(txt);
+
+		setForeground(Color.BLUE.darker());
+		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+		// Le agrega un escuchador al JLabel de tipo MouseListener para hacerlo clickable
+		addMouseListener(new Oyente());
+
+	}
+
+	public void setURL(String url) {
+		this.url = url;
+	}
+
+	private class Oyente extends MouseAdapter {
+		public void mouseClicked(MouseEvent evt) {
+			/* Comprueba si esta clase es compatible con la plataforma actual. Si es compatible, usa getDesktop() para recuperar una
+			 * instancia. */
+			Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+			// Si la accion especificada es compatible con la plataforma actual, entonces...
+			if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+				try {
+					desktop.browse(new URI(url));
+				} catch (URISyntaxException
+						/* Si la cadena dada viola RFC 2396, como se incrementa por las desviaciones anteriores. */ | NullPointerException
+						/* Si la cadena es nula. */ | IOException e) {
+					JOptionPane.showMessageDialog(JHyperlink.this, "No se pudo abrir el hipervínculo. Error: " + e.getMessage(), "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		}
+
+		// Cuando el mouse pasa sobre el hyperlink
+		public void mouseEntered(MouseEvent evt) {
+			setForeground(Color.BLUE.brighter());
+		}
+
+		// Cuando el mouse sale del hyperlink
+		public void mouseExited(MouseEvent evt) {
+			setForeground(Color.BLUE.darker());
+		}
+
+	}
+
+}
