859f07ff183f6c0109c4a7095a1c1e38b05c03ab
Juan Debenedetti <juandebenedetti94@gmail.com>
2020-11-23 02:06:47 -0300

    Agregando los .dat

--- src/main/java/com/silentsoft/calculadoraao/views/Principal.java
+++ src/main/java/com/silentsoft/calculadoraao/views/Principal.java
@@ -70,61 +70,6 @@ public class Principal extends JFrame {
 	// NPCs
 	private String[] npc, vida, expNPC, oro;
 
-	private Integer[][] datosPJ = { { 1, 150 }, { 2, 200 }, { 3, 250 }, { 4, 300 }, { 5, 350 }, { 6, 450 }, { 7, 550 }, { 8, 650 }, { 9, 750 },
-			{ 10, 1000 }, { 11, 1250 }, { 12, 1500 }, { 13, 1750 }, { 14, 2000 }, { 15, 2300 }, { 16, 2600 }, { 17, 2900 }, { 18, 3200 },
-			{ 19, 3500 }, { 20, 4000 }, { 21, 4500 }, { 22, 5000 }, { 23, 5900 }, { 24, 7350 }, { 25, 9150 }, { 26, 11400 }, { 27, 14250 },
-			{ 28, 17750 }, { 29, 22000 }, { 30, 27500 }, { 31, 35000 }, { 32, 42750 }, { 33, 52750 }, { 34, 62750 }, { 35, 72750 }, { 36, 85000 },
-			{ 37, 97250 }, { 38, 109500 }, { 39, 134500 }, { 40, 200000 }, { 41, 275000 }, { 42, 400000 }, { 43, 525000 }, { 44, 650000 },
-			{ 45, 765000 }, { 46, 955000 }, { 47, 1200000 }, { 48, 1500000 }, { 49, 2000000 } };
-
-	private Object[][] datosNPC = { { "Gallina salvaje", 20, 26, 10 }, { "Gallo salvaje", 20, 26, 10 }, { "Conejo", 25, 30, 11 },
-			{ "Serpiente", 25, 30, 11 }, { "Rana Venenosa", 30, 33, 14 }, { "Cuervo", 30, 33, 14 }, { "Murcielago", 30, 33, 14 },
-			{ "Escorpion", 35, 39, 16 }, { "Escorpion Oscuro", 35, 39, 16 }, { "Sertlac", 100, 70, 25 }, { "Rata Gigante", 115, 81, 29 },
-			{ "Mano poseida", 130, 91, 33 }, { "Señor de las Sombras", 150, 105, 38 }, { "Tortuga Gigante", 180, 126, 45 },
-			{ "Esqueleto", 200, 140, 50 }, { "Esqueleto Guardian", 300, 180, 60 }, { "Asesino", 350, 210, 70 }, { "Bandido", 360, 216, 72 },
-			{ "Mercenario", 375, 225, 75 }, { "Jabali Salvaje", 450, 225, 90 }, { "Zorro Rojo", 500, 250, 100 }, { "Zorro Gris", 500, 250, 100 },
-			{ "Lobo", 600, 300, 120 }, { "Tigre", 600, 300, 120 }, { "Oso Pardo", 750, 450, 113 }, { "Araña Gigante", 1000, 600, 150 },
-			{ "Orco", 1000, 600, 150 }, { "Orco Brujo", 1250, 750, 188 }, { "Jefe Orco", 2000, 1400, 500 }, { "Orco del Bosque", 1250, 750, 188 },
-			{ "Ent", 3000, 2100, 450 }, { "Ciclope", 3250, 2275, 488 }, { "Fango", 3500, 2450, 525 }, { "Basilisco", 3000, 2100, 450 },
-			{ "Aparicion", 4000, 3600, 1200 }, { "Ogro", 4500, 4050, 1350 }, { "Ogro Guerrero", 4750, 4275, 1425 },
-			{ "Ogro Lider", 7500, 7500, 2250 }, { "Arquero Pirata", 4000, 3600, 1200 }, { "Guerrero Pirata", 4500, 4050, 1350 },
-			{ "Jefe Pirata", 5000, 5000, 1500 }, { "Saqueador de Minas", 4250, 3825, 1275 }, { "Sirena", 5000, 5000, 1500 },
-			{ "Zombie Aullador", 560, 616, 196 }, { "Zombie Desquiciado", 555, 611, 194 }, { "Zombie Putrido", 550, 605, 193 },
-			{ "Zombie Mordedor", 575, 633, 201 }, { "Zombie Cenagoso", 595, 655, 208 }, { "Linche", 6500, 5850, 975 },
-			{ "Lord Orco", 6750, 6075, 1013 }, { "Yuan Ti", 7000, 6300, 1050 }, { "Hombre Lagarto", 7000, 6300, 1050 },
-			{ "Medusa", 8500, 8500, 1360 }, { "Medusa Matriarca", 10000, 10000, 1700 }, { "Golem de piedra", 20000, 16000, 2600 },
-			{ "Cria de Dragon Verde", 25000, 10000, 3500 }, { "Cria de Dragon Rojo", 27500, 11000, 3850 }, { "Conjuradora", 12500, 12500, 2125 },
-			{ "Golem de Bronce", 25000, 21250, 3500 }, { "Demonio", 15000, 7500, 3000 }, { "Demonio Abisal", 22500, 11250, 3150 },
-			{ "Golem de Plata", 40000, 36000, 5600 }, { "Neishtar", 12500, 12500, 2125 }, { "Tolkvar", 25000, 27500, 4500 },
-			{ "Oso Polar", 1050, 1155, 315 }, { "Huargo de Nieve", 1125, 1238, 338 }, { "Lobo Invernal", 975, 1073, 293 },
-			{ "Chaman Artico", 12500, 13750, 2500 }, { "Yeti", 50000, 65000, 10000 }, { "Golem de Hielo", 75000, 67500, 15000 },
-			{ "Cria de Rey Dragan", 35000, 17500, 7000 }, { "Corruptor", 995, 1095, 299 }, { "Turbador Sombrio", 1045, 1150, 314 },
-			{ "Lacayo del Mal", 1025, 1128, 308 }, { "Sanguijuela Gigante", 975, 1073, 293 }, { "Adosador de la Muerte", 12500, 13750, 2125 },
-			{ "Vampiro", 12500, 13750, 2125 }, { "Vampiresa", 12500, 13750, 2125 }, { "Vampiro Transformado", 95000, 109250, 19000 },
-			{ "Hechicero de Agua", 25000, 32500, 5000 }, { "Hechicero de Aire", 25000, 32500, 5000 }, { "Hechicero de Tierra", 25000, 32500, 5000 },
-			{ "Hechicero de Fuego", 25000, 32500, 5000 }, { "Profeta Arcano", 30000, 42000, 6000 }, { "Guardian", 30000, 42000, 6000 },
-			{ "Gran Hechicero Garveloth", 100000, 145000, 20000 }, { "Kraken", 12500, 15000, 4375 }, { "Golem de Oro", 60000, 60000, 9000 },
-			{ "Castigador", 17500, 17500, 2800 }, { "Conjuradora de Farzhe", 12500, 11250, 1875 }, { "Cria de Dragon Legendario", 20000, 9205, 7429 },
-			{ "Bruja", 1450, 547, 700 }, { "Mago Protector", 1400, 532, 678 }, { "Cria Aracnida", 645, 677, 226 }, { "Archne", 11500, 10305, 1840 },
-			{ "Desertor de las Tinieblas", 7500, 7500, 1275 }, { "Golem Infernal", 72000, 72000, 12240 }, { "F. Elemental Marron", 750, 825, 225 },
-			{ "F. Elemental V. Oscuro", 730, 803, 219 }, { "F. Elemental Fucsia", 710, 781, 213 }, { "F. Elemental Verde", 690, 759, 207 },
-			{ "F. Elemental Azul", 670, 737, 201 }, { "F. Elemental Gris", 650, 715, 195 }, { "F. Elemental Amarillo", 610, 671, 183 },
-			{ "Gran Dragon Verde", 175000, 113750, 52500 }, { "Gran Dragon Rojo", 190000, 123500, 57000 },
-			{ "Gran Dragon Azul", 225000, 146250, 67500 }, { "Gran Dragon Negro", 450000, 337500, 135000 },
-			{ "Dragon Legendario", 650000, 520000, 195000 }, { "Gran Dragon de Hielo", 750000, 600000, 225000 },
-			{ "Guarda del Pantano", 150000, 120000, 45000 }, { "Archimago Abisal", 175000, 210000, 52500 }, { "Gorgona", 125000, 150000, 37500 },
-			{ "Kern Ghard", 200000, 240000, 60000 }, { "Rey Dragon", 1000000, 1000000, 300000 }, { "Agnus el Oscuro", 3225, 3870, 645 },
-			{ "Momia enfurecida", 3750, 3350, 1200 }, { "Fantasma", 5300, 5565, 1060 }, { "Hombre Lobo", 6500, 7020, 1300 },
-			{ "Zombie Lobrego", 5000, 6000, 1000 }, { "Alma Cautiva", 16500, 18150, 3300 }, { "Ermitaño de Neiran", 12500, 16000, 2750 },
-			{ "Sacerdote de Neiran", 13050, 16704, 2871 }, { "Sabio de Neiran", 14275, 18272, 3141 }, { "Comandante de Neiran", 15750, 20475, 3465 },
-			{ "Sicario de Neiran", 25000, 33750, 5500 }, { "Cazador de Neiran", 27000, 36450, 5940 }, { "Archimago de Neiran", 30000, 43500, 6600 },
-			{ "Caballero de Neiran", 35000, 50750, 7700 }, { "Abigor", 25000, 38750, 6250 }, { "Belfegor", 34500, 54855, 8625 },
-			{ "Bali", 42750, 68288, 10688 }, { "Cerbero", 27500, 43175, 6875 }, { "Fuego Fatuo", 5500, 6325, 1100 },
-			{ "Elemental de Fuego", 8000, 10000, 1600 }, { "Elemental de Agua", 7750, 9688, 1550 }, { "Elemental de Tierra", 7500, 9000, 1500 },
-			{ "Hechizero de la Oscuridad", 12500, 16250, 3125 }
-
-	};
-
 	private static final short BONUS_10 = 10;
 	private static final short BONUS_25 = 25;
 	private static final short BONIFICADOR_EXP = 10;
@@ -152,7 +97,7 @@ public class Principal extends JFrame {
 	private void loadPJs() {
 		ini = new IniFile();
 		// Carga el dat de PJs
-		ini.load("dat" + SEPARADOR + "PJss.dat");
+		ini.load("dat" + SEPARADOR + "PJs.dat");
 
 		// Almacena los valores de las claves especificadas dentro de un array de String
 		nivel = ini.getValues("Nivel");
